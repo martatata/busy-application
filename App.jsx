49ec854d1a172378ee58aaf97a320729f0234919
Martacioli <martatacioli1@gmail.com>
2023-10-14 18:48:15 -0300

    autenticação

--- App.jsx
+++ App.jsx
@@ -23,7 +23,6 @@ export default function App() {
         <PersistGate persistor={persistor}>
           <MenuDrawer />
           </PersistGate>
-
         </NavigationContainer>
       </ProvedorAutenticacao>
     </Provider>
