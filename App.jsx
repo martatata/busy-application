e73acc45fef67ae4b3b299d30dae06bba394c235
Martacioli <martatacioli1@gmail.com>
2023-10-14 18:09:44 -0300

    arquivos de autenticação

--- App.jsx
+++ App.jsx
@@ -1,19 +1,32 @@
 // In App.js in a new project
 import 'react-native-gesture-handler';
 
-import * as React from 'react';
+import React , { useState, useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import MenuDrawer from './navigation/MenuDrawer'
 
+// importações referentes a autenticação
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './src/redux/store';
+import ProvedorAutenticacao from './Autentication/ProvedorAutenticacao'
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <MenuDrawer/>
-    </NavigationContainer>
+    <Provider store={store}>
+      <ProvedorAutenticacao>
+        <NavigationContainer>
+        <PersistGate persistor={persistor}>
+          <MenuDrawer />
+          </PersistGate>
+
+        </NavigationContainer>
+      </ProvedorAutenticacao>
+    </Provider>
   );
 }
+
